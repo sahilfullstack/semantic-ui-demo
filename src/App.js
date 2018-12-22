118ad6073c2675d14b7297603872381c3f5652ef
Sahil <sahilsarpal@gmail.com>
2018-12-22 18:29:37 +0530

    working good

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Grid, Dropdown } from 'semantic-ui-react'
 
 class App extends Component {
+
+  handleKeyDown(e) {
+      
+      let code = e.keyCode
+      console.log(code)
+      if(code == 13 || code == 38 || code == 40 ) {
+        e.target.click()
+      }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -19,6 +29,75 @@ class App extends Component {
           >
             Learn React
           </a>
+          <Grid columns={3}>
+    <Grid.Row>
+      <Grid.Column>
+        <Dropdown
+        placeholder='Select Friend' fluid selection options={[
+   {
+     text: 'Jenny Hess',
+     value: 'Jenny Hess'
+},{
+     text: 'SAhil Hess',
+     value: 'SAhil Hess',
+     active: true
+},{
+     text: 'Varun Hess',
+     value: 'Varun Hess'
+},{
+     text: 'Jen Hess',
+     value: 'Jen Hess'
+},{
+     text: 'ny Hess',
+     value: 'ny Hess'
+}]
+} />
+      </Grid.Column>
+      <Grid.Column>
+      <Dropdown  placeholder='Select Friend' fluid selection options={[
+   {
+     text: 'Jenny Hess',
+     value: 'Jenny Hess'
+},{
+     text: 'SAhil Hess',
+     value: 'SAhil Hess'
+},{
+     text: 'Varun Hess',
+     value: 'Varun Hess'
+},{
+     text: 'Jen Hess',
+     value: 'Jen Hess'
+},{
+     text: 'ny Hess',
+     value: 'ny Hess'
+}]
+} />
+
+
+      </Grid.Column>
+      <Grid.Column>
+                        <Dropdown placeholder='Select Friend' fluid selection options={[
+   {
+     text: '1 Jenny Hess',
+     value: 'Jenny Hess'
+},{
+     text: 'SAhil Hess',
+     value: 'SAhil Hess'
+},{
+     text: 'Varun Hess',
+     value: 'Varun Hess'
+},{
+     text: 'Jen Hess',
+     value: 'Jen Hess'
+},{
+     text: 'ny Hess',
+     value: 'ny Hess'
+}]
+} />
+      </Grid.Column>
+    </Grid.Row>
+  </Grid>
+
         </header>
       </div>
     );
